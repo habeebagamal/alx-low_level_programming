6778a337b3cb03bc4d00d5c06715e78448445818
habebagamal <habeebagamale@gmail.com>
2023-07-16 03:58:27 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - Prints the last digit of a randomly generated number
  * and whether it is greater than 5, less than 6, or 0.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -30,4 +30,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n",
 			n, n % 10);
 	}
+
+	return (0);
+
 }
