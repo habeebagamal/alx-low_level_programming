603f84f46ee7acaf8bf46990a02807f1a969223e
habebagamal <habeebagamale@gmail.com>
2023-07-16 03:53:12 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - Prints numbers from 0 to 9.
+ * main - Prints numbers from 0 to 9 and letters between a to f.
  *
  * Return: Always 0 (Success)
  */
@@ -10,7 +11,7 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		printchar(i);
+		putchar(i);
 	}
 	
 	for (i = 97; i < 103; i++)
