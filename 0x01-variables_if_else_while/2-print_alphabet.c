aaa2edaa120e8de0f203e3383b7f35406602fdff
habebagamal <habeebagamale@gmail.com>
2023-07-25 16:04:06 -0700

    0x01-variables_if_else_whil

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char alpha = 'a';
+        while (alpha<='z')
+	{ 
+		putchar(alpha);
+		alpha++;
+		putchar('\n');
 
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
 	}
-	putchar('\n');
-	return (0);
+	
+	
 }
