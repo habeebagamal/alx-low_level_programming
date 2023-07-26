07bad9801ba353f789649cabf02c77a2181a9b61
habebagamal <habeebagamale@gmail.com>
2023-07-26 15:01:55 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -1,23 +1,41 @@
-#include<stdio.h>
-#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
-{ 
-    char str1[12] = "Hello world";
-    char str3[12];
+{
+    char s1[98];
     char *ptr;
+    int i;
 
-
-   /* copy str1 into str3 */
-   ptr=strcpy(str3, str1);
-
-   printf("%s\n", ptr );
-   return 0;
-
-
-
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
+    printf("%s\n", s1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+    printf("%s\n", s1);
+    printf("%s\n", ptr);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+    printf("%s", s1);
+    printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
+    return (0);
 }
