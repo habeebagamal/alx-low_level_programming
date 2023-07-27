19f9726b690f066eb461abb7411d68f674e945a8
habebagamal <habeebagamale@gmail.com>
2023-07-27 08:41:04 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,23 +9,11 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-        int revar[12];
-
-
-        for(i=0;i<n;i++)
-         {
-            revar[i]=a[n-i-1];
-
-         }
-        for(int i=0; i<n; i++)
-        {
-        a[i] = revar[i];
-        }
-        printf("Reversed array: ");
-       for (int i = 0; i < n; i++)
-        {
-        printf("%d ", a[i]);
-        }
-       return 0;
-
+        int t;
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
 }
