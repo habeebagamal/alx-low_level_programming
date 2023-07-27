5fe6d9333bcde8bc6290733edf44186830bc9e93
habebagamal <habeebagamale@gmail.com>
2023-07-27 07:43:48 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,6 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	 int i;
-
-       while(s1[i] != '\0' && s2[i] != '\0')
-       {
-           if(s1[i]!=s2[i])
-           {
-               return(s1[i]-s2[i]);
-           }
-            i++;
-
-        }
-            return (0);
-
+	 while (*s1 && *s1 == *s2) { ++s1; ++s2; }
+    return (int)(unsigned char)(*s1) - (int)(unsigned char)(*s2);
 }
